16ff7bdfae52b07d4c22be2842e5c7695fce364f
Haoliang Zou <zouhaoliang@bupt.edu.cn>
2021-07-27 22:55:44 +0800

    Realize login module for the project.

--- app/users.py
+++ app/users.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, render_template, request
-from app.utils import checkExistence
+from app.utils import checkByEmail, checkByName, checkExistence
 from app.models import db, Users
 
 users = Blueprint("users", __name__)
 
 
-@users.route("/register", methods = ["POST", "GET"])
+@users.route("/register", methods=["POST", "GET"])
 def register():
     if request.method == "POST":
         name = request.form["name"]
@@ -28,3 +28,23 @@ def register():
             return render_template("register.html")
     else:
         return render_template("register.html")
+
+
+@users.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        mode = request.form["mode"]
+        identity = request.form["identity"]
+        password = request.form["password"]
+
+        errors = []
+        if mode == "name":
+            errors.extend(checkByName(identity, password))
+        else:
+            errors.extend(checkByEmail(identity, password))
+
+        if len(errors) > 0:
+            return render_template("login.html", errors=errors)
+        return render_template("login.html")
+    else:
+        return render_template("login.html")
--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,5 @@
 from app.models import Users
+from passlib.hash import bcrypt_sha256
 
 
 # used for name and email checks in a registration process
@@ -10,4 +11,26 @@ def checkExistence(name, email):
         errors.append("Your user name has been used. Pls use a new name.")
     if exist_email:
         errors.append("Your email has been registered. Pls try a new one.")
+    return errors
+
+
+# used for name and password checks when trying to login in with username
+def checkByName(name, password):
+    user = Users.query.filter_by(name=name).first()
+    errors = []
+    if user is None:
+        errors.append("No such user exists, pls check your inputs.")
+    elif not bcrypt_sha256.verify(str(password), user.password):
+        errors.append("Password does not match the given user. Pls try again.")
+    return errors
+
+
+# used for email and password checks when trying to login in with email
+def checkByEmail(email, password):
+    user = Users.query.filter_by(email=email).first()
+    errors = []
+    if user is None:
+        errors.append("No user with this email exists, pls check your inputs.")
+    elif not bcrypt_sha256.verify(str(password), user.password):
+        errors.append("Password does not match the given user. Pls try again.")
     return errors
