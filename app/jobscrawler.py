e943cb38e9fdf5a04f3410800cdc13d6228222be
Haoliang Zou <zouhaoliang@bupt.edu.cn>
2021-07-29 13:19:40 +0800

    Drew an outline of the web scrawler.

--- app/jobscrawler.py
+++ app/jobscrawler.py
@@ -13,5 +13,33 @@ def init_driver():
     return driver
 
 
-def init_jobposts():
+def init_jobposts(keyword, date=31):
     driver = init_driver()
+    pages = count_pages(driver, keyword, date)
+
+    for page in range(1, pages + 1):
+        web_content = get_webcontent(driver, keyword, date, page)
+        if web_content == None:
+            break
+        posts = extract_info(web_content)
+        create_jobposts(posts)
+
+
+# get the number of posts from the first page
+def count_pages(driver, keyword, date):
+    return
+
+
+# get all the contents in one page
+def get_webcontent(driver, keyword, date, page):
+    return
+
+
+# grab all the post information the content of one page
+def extract_info(content):
+    return 
+
+
+# create a new job post with the extracted information
+def create_jobposts(posts):
+    return
